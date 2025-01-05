208d908d4c857734c08629a54dcf8b3f73c16a39
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2025-01-05 18:48:06 +0600

    swiper added to category.

--- src/Pages/NavBar/NavBar.jsx
+++ src/Pages/NavBar/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
 
     </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar fixed  z-10 bg-opacity-30 bg-black max-w-screen-2xl text-white">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
